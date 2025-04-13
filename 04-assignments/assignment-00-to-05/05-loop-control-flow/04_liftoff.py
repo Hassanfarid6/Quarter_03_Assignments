aea56695879626ec78bb4c1b619107bd157b8b36
Ali_jan <hassanfarid667@gmail.com>
2025-04-13 07:37:58 +0500

    part 05 loop completed

---
+++ 04-assignments/assignment-00-to-05/05-loop-control-flow/04_liftoff.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    for i in range(10, 0, -1):  # Starts at 10, ends at 1, decrementing by 1
+        print(i, end="... ")  # Prints the countdown
+    print("Liftoff!")  # Prints Liftoff! after the countdown
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
