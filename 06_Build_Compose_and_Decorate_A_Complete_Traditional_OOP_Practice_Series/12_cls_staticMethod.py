fcd3d312fdcc58e72dd9ca792b73ffd11493bcab
Ali_jan <hassanfarid667@gmail.com>
2025-05-19 22:05:28 +0500

    11,12 completed

---
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/12_cls_staticMethod.py
@@ -0,0 +1,23 @@
+# 12. Static Methods Assignment:
+# Create a class TemperatureConverter with a static method celsius_to_fahrenheit(c)
+# that returns the Fahrenheit value.
+
+
+
+
+class TemperatureConverter:
+
+
+    @staticmethod
+    def celsius_to_fahrenheit(c):
+
+        return (c * 9/5) + 32
+    
+    @staticmethod
+    def fahrenheit_to_celsius(c):
+
+        return (c -32) * 5/9
+    
+res = TemperatureConverter()
+print(f"Temperature in fahrenheit ==  {res.celsius_to_fahrenheit(40)} °f")
+print(f"Temperature in Celsius ==  {res.fahrenheit_to_celsius(104)} °C")
