aa6919ec5093f941b314d229c9018a1b02c87998
Ali_jan <hassanfarid667@gmail.com>
2025-05-22 06:59:05 +0500

    project completed

--- 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/06_cls_constructor.py
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/06_cls_constructor.py
@@ -1,6 +1,6 @@
 # 6. Constructors and Destructors Assignment:
-# Create a class Logger that prints a message when an object is created (constructor) and another message
-# when it is destroyed (destructor).
+# Create a class Logger that prints a message when an object is created (constructor)
+# and another message when it is destroyed (destructor).
 
 class Logger: 
     def __init__(self, name):
